f5a23f2452373fcc3965e3ee3fc15f519e28c40f
unknown <snamfukwe@g.harvard.edu>
2014-09-19 01:43:52 -0700

    First submission for Project 1

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Sibo Namfukwe</title>
+
+
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+
+	
+</head>
+
+
+<body>	
+		<div class="pure-u">
+
+		<h1>Sibo Namfukwe </br> CSCI E-15 </h1>
+		<img class="pure-img" src="img/me.png">
+		
+		<h2>P2</h2>
+		<a href="">Live URL</a> </br>
+		<a href="">Github URL</a>
+		
+		<h2>P3</h2>
+		<a href="">Live URL</a> </br>
+		<a href="">Github URL</a>
+		
+		<h2>P4</h2>
+		<a href="">Live URL</a> </br>
+		<a href="">Github URL</a>
+	
+		</div>
+
+
+		<div>
+
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>Hi. I'm Sibo, a Digital Developer. I have been building relatively simple static web pages via the adobe muse platform.</p>		
+
+		<h2>Why are you taking this course?</h2>
+		<p>I'm taking this course to expand my skills and referesh on html/css/javascipt.</p>
+
+		<h2>What previous experience do you have with programming?</h2>
+		<p>I have a background in web development academically but haven't had much opportunity to use those skills at work. </p>
+		<p>I am anticipating having to build a dynamic website in the not too distant future so I am hoping to get a refresher and to learn a lot of new valuable skills that I can easily transfer into the work place.</p>
+		
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I'm working on a pc.</p>
+
+		<h2>Anything else we should know?</h2>
+		<p>I'm really excited about this class! I'm excited about our instructor and everything I'll be able to learn. I am loving it so far. It's going to be challenging for me but it's exactly what I need at this moment in my career.</p>
+
+		</div>
+</br>
+</br>
+</br>
+		
+</body>
+</html>
